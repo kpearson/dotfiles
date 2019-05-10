84d271513d514d0104ef5cf4a3687f2871c3d157
Kit Pearson <dev_kit@icloud.com>
2019-05-10 15:55:14 -0700

    Add Yarn autocompletions

---
+++ shell/oh-my-zsh-custom/plugins/yarn-autocompletions/yarn-autocompletions.plugin.zsh
@@ -0,0 +1,34 @@
+YARN_AUTO_COMP_PATH="$(dirname $0)/yarn-autocompletions"
+
+function _fetch_yarn_autocompletions_result () {
+    compls=$($YARN_AUTO_COMP_PATH $1)
+    completions=(${=compls})
+    compadd -- $completions
+}
+
+function _yarn_autocompletions () {
+    case $words[2] in
+        add)
+            if [[ $words[3] == "--dev" ]]
+            then
+                _fetch_yarn_autocompletions_result "add-dev"
+            else
+                _fetch_yarn_autocompletions_result "add"
+            fi
+            ;;
+        remove)
+            _fetch_yarn_autocompletions_result "remove"
+            ;;
+        upgrade)
+            _fetch_yarn_autocompletions_result "remove"
+            ;;
+        why)
+            _fetch_yarn_autocompletions_result "why"
+            ;;
+        *)
+            _fetch_yarn_autocompletions_result "scripts"
+            ;;
+    esac
+}
+
+compdef _yarn_autocompletions yarn
